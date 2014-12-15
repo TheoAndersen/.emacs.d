8bf5705d1966b7fe67e856de573fe603378a5039
Theo Andersen Carton <theo.andersen.carton@visma.com>
2014-12-15 11:39:57 +0100

    Added nostartup + nomenu etc to beginning of init.el

--- init.el
+++ init.el
@@ -3,6 +3,15 @@
 ;;  Initial Setup  |
 ;;_________________|
 
+; no splash screen
+(setq inhibit-startup-message t)
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+
 ; is mac?
 (setq is-mac (equal system-type 'darwin))
 
