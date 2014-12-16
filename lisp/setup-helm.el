c58065778dfc3029de773680a4a059bab7abf047
Theo Andersen Carton <andersen.theo@gmail.com>
2014-12-16 22:15:02 +0100

    Changed helm mapping to include in both windows and mac

--- lisp/setup-helm.el
+++ lisp/setup-helm.el
@@ -16,11 +16,9 @@
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
 
-(when is-mac
-  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-  (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-  (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-  )
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
 (when (executable-find "ack-grep")
   (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
