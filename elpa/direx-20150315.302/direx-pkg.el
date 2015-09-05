7ad1e0037e48c657db784df6269bffc042e90672
Theo Andersen Carton <andersen.theo@gmail.com>
2015-09-05 13:26:15 +0200

    Updated packages

--- elpa/direx-20150315.302/direx-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "direx" "20150315.302" "Simple Directory Explorer" 'nil :keywords ("convenience"))
--- elpa/git-link-20150722.1704/git-link-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "git-link" "20150722.1704" "Get the GitHub/Bitbucket/Gitorious URL for a buffer location" 'nil :url "http://github.com/sshaw/git-link" :keywords '("git"))
