e82830621278b71d7403a97d24e7ed7e2c5ba7c9
Theo Andersen Carton <andersen.theo@gmail.com>
2015-09-29 22:17:38 +0200

    Changed packages to melpa-stable

--- elpa/color-theme-wombat-0.0.1/color-theme-wombat-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; color-theme-wombat-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("color-theme-wombat.el") (21637 32912
-;;;;;;  420263 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; color-theme-wombat-autoloads.el ends here
--- elpa/color-theme-wombat-0.0.1/color-theme-wombat-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "color-theme-wombat" "0.0.1" "The wombat color theme for Emacs." '((color-theme "6.6.1")))
