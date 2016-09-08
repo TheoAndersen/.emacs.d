5e7a5ca9874426c31f8b7649e02bf8224ef9fd19
Theo Andersen Carton <andersen.theo@gmail.com>
2016-09-08 13:10:38 +0200

    Update packages

--- elpa/transcribe-1.5.0/transcribe-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; transcribe-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "transcribe" "transcribe.el" (22378 17489 0
-;;;;;;  0))
-;;; Generated autoloads from transcribe.el
-
-(autoload 'transcribe-mode "transcribe" "\
-Toggle transcribe-mode
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; transcribe-autoloads.el ends here
---
+++ elpa/transcribe-1.5.2/transcribe-pkg.el
@@ -0,0 +1,1 @@
+(define-package "transcribe" "1.5.2" "Package for audio transcriptions" 'nil :url "http://elpa.gnu.org/packages/transcribe.html" :keywords nil)
