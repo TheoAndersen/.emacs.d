a741092366f3cd146f4ffd1fee73f7319bb22633
Theo Andersen Carton <andersen.theo@gmail.com>
2017-10-10 23:22:04 +0200

    add telephone-line which looks nice for evil as well

--- custom.el
+++ custom.el
@@ -19,8 +19,9 @@
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (pyenv-mode-auto smooth-scroll company-jedi virtualenvwrapper rg ripgrep prettier-js counsel leuven-theme spaceline-all-the-icons all-the-icons py-autopep8 ein helm-swoop dockerfile-mode vlf org-present zoom-frm zenburn-theme web-mode use-package undo-tree tss transcribe tide smooth-scrolling smartparens sass-mode rainbow-delimiters omnisharp neotree multiple-cursors markdown-mode magit js2-mode helm-projectile helm-ag golden-ratio git-timemachine fsharp-mode flycheck-pos-tip flycheck-elm feature-mode expand-region emms elm-mode elixir-yasnippets elisp-slime-nav ecukes company-tern alchemist ace-link ace-jump-zap ace-jump-buffer ac-ispell)))
+    (telephone-line pyenv-mode-auto smooth-scroll company-jedi virtualenvwrapper rg ripgrep prettier-js counsel leuven-theme spaceline-all-the-icons all-the-icons py-autopep8 ein helm-swoop dockerfile-mode vlf org-present zoom-frm zenburn-theme web-mode use-package undo-tree tss transcribe tide smooth-scrolling smartparens sass-mode rainbow-delimiters omnisharp neotree multiple-cursors markdown-mode magit js2-mode helm-projectile helm-ag golden-ratio git-timemachine fsharp-mode flycheck-pos-tip flycheck-elm feature-mode expand-region emms elm-mode elixir-yasnippets elisp-slime-nav ecukes company-tern alchemist ace-link ace-jump-zap ace-jump-buffer ac-ispell)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(telephone-line-mode t)
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map
    (quote
@@ -48,4 +49,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 180 :family "Inconsolata" :weight medium)))))
+ '(default ((t (:height 180 :family "Inconsolata" :weight medium))))
+ '(telephone-line-evil-normal ((t (:inherit telephone-line-evil :background "forest green"))))
+ '(telephone-line-evil-operator ((t (:inherit telephone-line-evil :background "forest green")))))
